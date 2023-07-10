88a5dc9dba37044fdf42a7723a187e460e44bc88
CTAR45049 <tanutsuparat@gmail.com>
2023-07-10 16:24:49 +0700

    555

---
+++ FirstLoop_1.py
@@ -0,0 +1,5 @@
+x = int(input(''))
+result = 0
+for i in range(x):
+    result = result+(i+1)
+print(result)
---
+++ FirstLoop_3.py
@@ -0,0 +1,4 @@
+A = int(input(''))
+for number in range(1, A):
+    if number % 2 == 1:
+        print(number)
